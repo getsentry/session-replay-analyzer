7b2d65a7ec5a7b075befcf26ade5bf1caacffdd8
Colton Allen <colton.allen@sentry.io>
2023-11-01 11:50:42 -0500

    Add sentry integration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { app } from './app'
+import * as Sentry from "@sentry/node";
+
+const sentry_dsn = process.env.SENTRY_DSN || ""
+const sample_rate = parseFloat(process.env.SENTRY_TRACE_SAMPLE_RATE || "0")
+
+if (sentry_dsn !== "") {
+    Sentry.init({
+        dsn: sentry_dsn,
+        tracesSampleRate: sample_rate,
+    });
+}
 
 const port = 3000
 app.listen(port, () => { console.log(`Running forever at http://localhost:${port}`) })
