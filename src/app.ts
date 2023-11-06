31e616d3f44c4d4ff1698edcf166ebcee9df9d4e
Colton Allen <colton.allen@sentry.io>
2023-11-06 15:14:00 -0600

    Move sentry init to app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,23 @@ import { runA11Y } from './accessibility'
 import { newPlayerPage } from './player'
 import { newStorage } from './gcs'
 import * as Sentry from "@sentry/node";
+import {SENTRY_DSN, SENTRY_TRACE_SAMPLE_RATE, SENTRY_PROFILE_SAMPLE_RATE} from './config'
+import { ProfilingIntegration } from "@sentry/profiling-node";
 
 const storage = newStorage()
-
 const app = express()
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Sentry.Integrations.Express({ app }),
+      new ProfilingIntegration(),
+  ],
+  tracesSampleRate: SENTRY_TRACE_SAMPLE_RATE,
+  profilesSampleRate: SENTRY_PROFILE_SAMPLE_RATE,
+});
+
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json())
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,4 @@
 import { app } from './app'
-import {SENTRY_DSN, SENTRY_TRACE_SAMPLE_RATE, SENTRY_PROFILE_SAMPLE_RATE} from './config'
-import * as Sentry from "@sentry/node";
-import { ProfilingIntegration } from "@sentry/profiling-node";
-
-
-Sentry.init({
-    dsn: SENTRY_DSN,
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Sentry.Integrations.Express({ app }),
-        new ProfilingIntegration(),
-    ],
-    tracesSampleRate: SENTRY_TRACE_SAMPLE_RATE,
-    profilesSampleRate: SENTRY_PROFILE_SAMPLE_RATE,
-});
-
 
 const port = 3000
 app.listen(port, () => { console.log(`Running forever at http://localhost:${port}`) })
